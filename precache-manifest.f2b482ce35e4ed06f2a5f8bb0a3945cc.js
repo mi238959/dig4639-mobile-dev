b738cd410886205dbdaebd0a4c5f7d1cebf8c119
mi238959 <59659041+mi238959@users.noreply.github.com>
2020-02-12 11:43:56 -0500

    Updates

---
+++ precache-manifest.f2b482ce35e4ed06f2a5f8bb0a3945cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cdd0008f8267ebc7da0be6d84ac90c8",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "cb750071b06387c622fe",
+    "url": "/dig4639-mobile-dev/static/css/main.2a34271c.chunk.css"
+  },
+  {
+    "revision": "0d0ae6ee3ad0d33d12a4",
+    "url": "/dig4639-mobile-dev/static/js/2.a0b43dfa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.a0b43dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb750071b06387c622fe",
+    "url": "/dig4639-mobile-dev/static/js/main.7f73fccc.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
